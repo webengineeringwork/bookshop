a53f6314564858e478e265d34fe14b39d3cd7c0f
Bosssheep <389137297@qq.com>
2018-07-04 23:24:45 +0800

    Admin Home Controller

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -12,8 +12,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
     <!-- Styles -->
-    <link href="//cdn.shopify.com/s/files/1/0880/2454/t/2/assets/timber.scss.css?10061947258110571957" rel="stylesheet" type="text/css" media="all" />
-    <link href="//cdn.shopify.com/s/files/1/0880/2454/t/2/assets/theme.scss.css?10061947258110571957" rel="stylesheet" type="text/css" media="all" />
+    @yield('style')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
@@ -104,6 +103,16 @@
     </nav>
 
     <!-- JavaScripts -->
+    <script type="text/javascript">
+        var uploadField = document.getElementById("image");
+
+        uploadField.onchange = function() {
+            if(this.files[0].size > 50000){
+                alert("File is too big!");
+                this.value = "";
+            };
+        };
+    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
